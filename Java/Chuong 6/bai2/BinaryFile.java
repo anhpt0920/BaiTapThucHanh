06bfc46a573a0d936f4a889a00c8861b334f1b6b
anhpt0920 <panhtokusatsu@gmail.com>
2024-11-22 11:39:48 +0700

    Update BinaryFile.java

--- Java/Chuong 6/bai2/BinaryFile.java
+++ Java/Chuong 6/bai2/BinaryFile.java
@@ -1,5 +1,39 @@
-package Java.Chuong 6.Bai 2;
-
+package bai2;
+import java.io.*;
+import java.util.Scanner;
 public class BinaryFile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập một chuỗi ký tự: ");
+        String str = sc.nextLine();
+        System.out.print("Nhập một ký tự: ");
+        char c = sc.next().charAt(0);
+        System.out.print("Nhập một số nguyên: ");
+        int num = sc.nextInt();
+        System.out.print("Nhập một số thực: ");
+        double d = sc.nextDouble();
 
+        // Ghi vào file nhị phân
+        try (DataOutputStream dos = new DataOutputStream(new FileOutputStream("exp.dat"))) {
+            dos.writeUTF(str);
+            dos.writeChar(c);
+            dos.writeInt(num);
+            dos.writeDouble(d);
+        } 
+        catch (IOException e) {
+            System.out.println("Lỗi khi ghi file: " + e.getMessage());
+        }
+
+        // Đọc dữ liệu từ file nhị phân
+        try (DataInputStream dis = new DataInputStream(new FileInputStream("exp.dat"))) {
+            System.out.println("Dữ liệu từ file:");
+            System.out.println(dis.readUTF());
+            System.out.println(dis.readChar());
+            System.out.println(dis.readInt());
+            System.out.println(dis.readDouble());
+        }
+        catch (IOException e) {
+            System.out.println("Lỗi khi đọc file: " + e.getMessage());
+        }
+    }
 }
