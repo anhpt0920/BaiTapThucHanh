7caa09c3e1445092b680a0c3f5edc8fb02e0877d
anhpt0920 <panhtokusatsu@gmail.com>
2024-11-24 09:08:55 +0700

    Add files via upload

---
+++ Java/Chuong 3/Bai11/HocVienDH.java
@@ -0,0 +1,42 @@
+package bai10;
+
+import java.util.Scanner;
+
+public class HocVienDH extends HocVien{
+    protected int soBuoi;
+    protected double donGiaDH;
+    protected int loaiUuTien;
+
+    public HocVienDH() {
+        super();
+    }
+
+    public HocVienDH(int soBuoi, double donGiaDH, int loaiUuTien) {
+        this.soBuoi = soBuoi;
+        this.donGiaDH = donGiaDH;
+        this.loaiUuTien = loaiUuTien;
+    }
+
+    public HocVienDH(int soBuoi, double donGiaDH, int loaiUuTien, String hoTen, String diaChi, String loaiCT) {
+        super(hoTen, diaChi, loaiCT);
+        this.soBuoi = soBuoi;
+        this.donGiaDH = donGiaDH;
+        this.loaiUuTien = loaiUuTien;
+    }
+    public void nhapThongTin(){
+        Scanner sc = new Scanner(System.in);
+        super.nhapThongTin();
+        System.out.printf("Nhap so buoi: "); soBuoi = sc.nextInt();
+        System.out.printf("Nhap don gia DH: "); donGiaDH = sc.nextDouble();
+        System.out.printf("Nhap loai uu tien: "); loaiUuTien = sc.nextInt();
+    }
+
+    @Override
+    public double hocPhi() {
+        double UuTien;
+        if(loaiUuTien == 1) UuTien = 1000000;
+        else UuTien = 500000;
+        return soBuoi * donGiaDH - UuTien;
+    }
+    
+}
